d0d12b92ab81451653acd2e3c495b855a4bf7270
Dhairya <dhairya.chugh77@gmail.com>
2020-06-15 02:10:40 +0530

    Update final.tf

--- final.tf
+++ final.tf
@@ -3,16 +3,37 @@ provider "aws" {
   profile = "dhairya"
 }
 
-resource "tls_private_key" "this" {
-  algorithm = "RSA"
+ resource "aws_instance" "myin" {
+  ami           = "ami-0447a12f28fddb066"
+  instance_type = "t2.micro"
+  availability_zone = "ap-south-1a"
+  key_name = "test"
+  vpc_security_group_ids = [aws_security_group.allow_http.id]
+  
+   connection {
+    type     = "ssh"
+    user     = "ec2-user"
+   private_key = file("C:/Users/dhair/Downloads/test.pem")
+	port = 22
+    host     = aws_instance.myin.public_ip
+  }
+   provisioner "remote-exec" {
+    inline = [
+      "sudo yum install httpd php git -y",
+      "sudo systemctl start httpd",
+	  "sudo systemctl enable httpd",
+	  
+    ]
+	
+  }
+
+
+  tags = {
+    Name = "dhairyaos"
+  }
 }
 
-module "key_pair" {
-  source = "terraform-aws-modules/key-pair/aws"
 
-  key_name   = "my-tera-key"
-  public_key = tls_private_key.this.public_key_openssh
-}
 
 resource "aws_security_group" "allow_http" {
   name        = "allow_http"
@@ -33,6 +54,12 @@ resource "aws_security_group" "allow_http" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+   egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
 
   tags = {
@@ -41,79 +68,132 @@ resource "aws_security_group" "allow_http" {
 }
 
 
-
 resource "aws_ebs_volume" "external_volume" {
-  availability_zone = "ap-south-1a"
+  availability_zone = aws_instance.myin.availability_zone
   size              = 1
 }
 resource "aws_volume_attachment" "ebs_attach" {
   device_name = "/dev/sdf"
-  volume_id   = "${aws_ebs_volume.external_volume.id}"
-  instance_id = "${aws_instance.myin.id}"
+  volume_id   = aws_ebs_volume.external_volume.id
+  instance_id = aws_instance.myin.id
   force_detach =true
 }
 
-
-  resource "aws_instance" "myin" {
-  ami           = "ami-0447a12f28fddb066"
-  instance_type = "t2.micro"
-  availability_zone = "ap-south-1a"
-  key_name = "my-tera-key"
-  user_data = "${file("install.sh")}"
-  vpc_security_group_ids = ["${aws_security_group.allow_http.name}"]
-
-
-  tags = {
-    Name = "dhairyaos"
-  }
+output "myos_ip" {
+  value = aws_instance.myin.public_ip
+}
+resource "null_resource" "nulllocal2"  {
+	provisioner "local-exec" {
+	    command = "echo  ${aws_instance.myin.public_ip} > publicip.txt"
+  	}
 }
 
+resource "null_resource" "execute" {
 
+depends_on =[
+aws_volume_attachment.ebs_attach,
+]
+connection {
+    type     = "ssh"
+    user     = "ec2-user"
+     private_key = file("C:/Users/dhair/Downloads/test.pem")
+	 port=22
+    host     = aws_instance.myin.public_ip
+  }
 
+ provisioner "remote-exec" {
+    inline = [
+      "sudo mkfs.ext4 /dev/xvdf",
+	  "sudo mount /dev/xvdf /var/www/html",
+	  "sudo rm -rf /var/www/html/*",
+	  "sudo git clone https://github.com/dhairya2019/test1.git /var/www/html/"
+    ]
+  }
+ 
 
+}
+
+ 
 resource "aws_s3_bucket" "_500069956" {
-  bucket = "mybestbucket77"
-  acl    = "private"
 
-  tags = {
+depends_on =[
+aws_instance.myin,
+]
+  bucket = "mybestbucket77"
+  acl    = "public-read"
+  
+   provisioner "local-exec" {
+        command     = "mkdir hello"
+	
+    }
+	
+  provisioner "local-exec" {
+        command     = "git clone https://github.com/dhairya2019/bucket hello"
+	
+    }
+	
+	provisioner "local-exec"{
+	
+	when = destroy
+	command     =   "echo Y | rmdir /s hello"
+	}
+	
+	
+	 tags = {
     Name = "your_bucket_500069956"
   }
+	}
+
+	
+  resource "aws_s3_bucket_object" "upload" {
+  depends_on =[
+aws_s3_bucket._500069956,
+]
+  bucket = aws_s3_bucket._500069956.id
+  key    = "1.png"
+  source = "hello/1.png"
+  content_type ="image/png"
+  acl="public-read"
 }
+
 
 locals {
   s3_origin_id = "myS3Origin"
 }
 
 resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+
+depends_on =[
+aws_s3_bucket_object.upload,
+]
   comment = "Some comment"
 }
 
 
-
-
-
-
 resource "aws_cloudfront_distribution" "s3_distribution" {
+depends_on =[
+aws_cloudfront_origin_access_identity.origin_access_identity,
+]
   origin {
-    domain_name = "${aws_s3_bucket._500069956.bucket_regional_domain_name}"
-    origin_id   = "${local.s3_origin_id}"
+    domain_name = aws_s3_bucket._500069956.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
 
   
 	s3_origin_config {
-  origin_access_identity = "${aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path}"
+  origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
 }
   }
 
   enabled             = true
   is_ipv6_enabled     = true
   comment             = "Some comment"
-  default_root_object = "dashboard.png"
+  default_root_object = "1.png"
 
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "${local.s3_origin_id}"
+    target_origin_id = local.s3_origin_id
 
     forwarded_values {
       query_string = false
@@ -129,50 +209,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     max_ttl                = 86400
   }
 
-  # Cache behavior with precedence 0
-  ordered_cache_behavior {
-    path_pattern     = "/content/immutable/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = "${local.s3_origin_id}"
-
-    forwarded_values {
-      query_string = false
-      headers      = ["Origin"]
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
-
-  # Cache behavior with precedence 1
-  ordered_cache_behavior {
-    path_pattern     = "/content/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "${local.s3_origin_id}"
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
+ 
 
   price_class = "PriceClass_200"
 
@@ -190,6 +227,24 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   viewer_certificate {
     cloudfront_default_certificate = true
   }
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+     private_key = file("C:/Users/dhair/Downloads/test.pem")
+	 port=22
+    host     = aws_instance.myin.public_ip
+  }
+  
+  provisioner "remote-exec" {
+        inline  = [
+		# "sudo su << \"EOF\" \n echo \"<img src='${self.domain_name}'>\" >> /var/www/html/index.html \n \"EOF\""
+ "sudo su << EOF",
+            "echo \"<img src='http://${self.domain_name}/${aws_s3_bucket_object.upload.key}'>\" >> /var/www/html/index.html",
+            "EOF"
+			]
+			}
+
+  
 }
 
 data "aws_iam_policy_document" "s3_policy" {
@@ -222,4 +277,17 @@ resource "aws_s3_bucket_policy" "_500069956" {
 
 output "description_ec2" {
 value = aws_instance.myin
+}
+
+resource "null_resource" "local_execute" {
+depends_on = [
+    aws_cloudfront_distribution.s3_distribution,
+  ]
+
+
+provisioner "local-exec" {
+   command = "start chrome ${aws_instance.myin.public_ip}"
+  }
+
+
 }
